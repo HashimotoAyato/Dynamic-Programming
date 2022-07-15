e471c0daff81980664e686fde20a7d70877e3b61
HashimotoAyato <ayaenum@gmail.com>
2022-07-16 03:53:32 +0900

    add main1 another file

---
+++ main1_DP_Forward.py
@@ -0,0 +1,21 @@
+# For文が順方向となるような解法
+
+import numpy as np
+
+n = int(input())
+wv = list(map(int, input().split()))
+w, v = wv[0::2], wv[1::2]
+W = int(input())
+
+dp = np.zeros((n+1, W+1), dtype = int)
+
+for i in range(n):
+    for j in range(W+1):
+        # 入らない
+        if j < w[i]:
+            dp[i+1,j] = dp[i,j]
+        # 入れる場合と入れない場合で比べる
+        else:
+            dp[i+1,j] = max(dp[i,j], dp[i,j-w[i]] + v[i])
+
+print(dp[n,W])
